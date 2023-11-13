99d4a9c72cbebbea5c1a6c78389225c4c2a2d7eb
yjb23tech <yjb23tech@gmail.com>
2023-11-13 14:37:52 +0000

    Doing good kiddo

--- scrimba/Learn_React/second_react/index.js
+++ scrimba/Learn_React/second_react/index.js
@@ -1,9 +1,10 @@
 const App = () => {
 
-    return React.createElement("div", {}, React.createElement("h3", {}, "Prometheus Risen."));
+    return React.createElement("div", {}, React.createElement("li", {}, "Prometheus Risen."), React.createElement("li", {}, "Apotheosis"), React.createElement("li", {}, "El Magico"));
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
+
 
