d97ee3732293ea7c84dc1fda52b7a5d140efccae
yjb23tech <yjb23tech@gmail.com>
2023-11-13 16:56:07 +0000

    Go To Hell

--- scrimba/Learn_React/second_react/index.js
+++ scrimba/Learn_React/second_react/index.js
@@ -1,6 +1,9 @@
 const App = () => {
 
-    return React.createElement("div", {}, React.createElement("li", {}, "Prometheus Risen."), React.createElement("li", {}, "Apotheosis"), React.createElement("li", {}, "El Magico"));
+    return React.createElement("div", {}, [
+        React.createElement("li", {}, "Prometheus Risen."), 
+        React.createElement("li", {}, "Apotheosis"), 
+        React.createElement("li", {}, "Los Blancos")]);
 };
 
 const container = document.getElementById("root");
